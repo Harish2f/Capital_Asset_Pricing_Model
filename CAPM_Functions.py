38ba1e38e3fffdf97e2e1e862e1b807622789c42
Harish kumar <harish2f@gmail.com>
2023-05-16 00:29:49 +0530

    updated alpha, beta, CAPM calculations

--- CAPM_Functions.py
+++ CAPM_Functions.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import numpy as np
 
 # function to plot an interactive chart
 def interactive_plot(df):
@@ -15,7 +16,24 @@ def interactive_plot(df):
     return fig
 
 # function to normalize stock prices based on the initial prices
-def normalize(df):
+def normalize(df_2):
+    df = df_2.copy()
     for column in df.columns[1:]:
         df[column] = df[column] / df[column][0]
     return df
+
+# function to calculate daily returns
+def daily_return(df):
+    df_daily_return = df.copy()
+    for i in df.columns[1:]:
+        for j in range(1, len(df)):
+            df_daily_return[i][j] = ((df[i][j] - df[i][j-1])/df[i][j-1]) * 100
+    df_daily_return[i][0] = 0
+    return df_daily_return
+
+# function to calculate beta
+def calculate_beta(stocks_daily_return, stock):
+    rm = stocks_daily_return['sp500'].mean()*252
+    
+    b,a = np.polyfit(stocks_daily_return['sp500'], stocks_daily_return[stock],1)
+    return b,a
--- CAPM_Retun.py
+++ CAPM_Retun.py
@@ -6,7 +6,7 @@ import yfinance as yf
 import pandas_datareader.data as web
 import datetime
 import CAPM_Functions
-import plotly
+import plotly.graph_objects as go
 
 st.set_page_config(page_title = "CAPM",
                    page_icon = "chart_with_upwards_trend",
@@ -23,36 +23,76 @@ with col1:
 with col2:
     year = st.number_input("Number of Years", 1, 10)
 
-end = datetime.date.today()
-start = datetime.date(end.year - int(year), end.month, end.day)
-SP500 = web.DataReader(['sp500'], 'fred', start, end)
+try:
+    end = datetime.date.today()
+    start = datetime.date(end.year - int(year), end.month, end.day)
+    SP500 = web.DataReader(['sp500'], 'fred', start, end)
 
-stocks_df = pd.DataFrame()
+    stocks_df = pd.DataFrame()
 
-for stock in stocks_list:
-    data = yf.download(stock, period = f'{year}y')
-    stocks_df[f'{stock}'] = data['Close']
+    for stock in stocks_list:
+        data = yf.download(stock, period = f'{year}y')
+        stocks_df[f'{stock}'] = data['Close']
+        
+    stocks_df.reset_index(inplace= True)
+    SP500.reset_index(inplace= True)
+    SP500.columns = ['Date', 'sp500']
+    stocks_df['Date'] = stocks_df['Date'].astype('datetime64[ns]')
+    stocks_df['Date'] = stocks_df['Date'].apply(lambda x:str(x)[:10])
+    stocks_df['Date'] = pd.to_datetime(stocks_df['Date'])
+    stocks_df = pd.merge(stocks_df, SP500, on = 'Date', how = 'inner')
+
+    col1, col2 = st.columns([1,1])
+    with col1:
+        st.markdown('### Dataframe Head')
+        st.dataframe(stocks_df.head(), use_container_width= True)
+    with col2:
+        st.markdown('### Dataframe Tail')
+        st.dataframe(stocks_df.tail(), use_container_width= True)
+
+    col1, col2 = st.columns([1,1])
+    with col1:
+        st.markdown('### Price of all Stocks')
+        st.plotly_chart(CAPM_Functions.interactive_plot(stocks_df))
+    with col2:
+        st.markdown('### Price of all Stocks(After Normalizing)')
+        st.plotly_chart(CAPM_Functions.interactive_plot(CAPM_Functions.normalize(stocks_df)))
+        
+    stocks_daily_return = CAPM_Functions.daily_return(stocks_df)
+
+    beta = {}
+    alpha = {}
+
+    for i in stocks_daily_return.columns:
+        if i != 'Date' and i != 'sp500':
+            b,a = CAPM_Functions.calculate_beta(stocks_daily_return, i)
+            
+            beta[i] = b
+            alpha[i] = a
+            
+    beta_df =pd.DataFrame(columns = ['Stock', 'Beta Value'])
+    beta_df['Stock'] = beta.keys()
+    beta_df['Beta Value'] = [str(round(i,2)) for i in beta.values()]
     
-stocks_df.reset_index(inplace= True)
-SP500.reset_index(inplace= True)
-SP500.columns = ['Date', 'sp500']
-stocks_df['Date'] = stocks_df['Date'].astype('datetime64[ns]')
-stocks_df['Date'] = stocks_df['Date'].apply(lambda x:str(x)[:10])
-stocks_df['Date'] = pd.to_datetime(stocks_df['Date'])
-stocks_df = pd.merge(stocks_df, SP500, on = 'Date', how = 'inner')
-
-col1, col2 = st.columns([1,1])
-with col1:
-    st.markdown('### Dataframe Head')
-    st.dataframe(stocks_df.head(), use_container_width= True)
-with col2:
-    st.markdown('### Dataframe Tail')
-    st.dataframe(stocks_df.tail(), use_container_width= True)
-
-col1, col2 = st.columns([1,1])
-with col1:
-    st.markdown('### Price of all Stocks')
-    st.plotly_chart(CAPM_Functions.interactive_plot(stocks_df))
-with col2:
-    st.markdown('### Price of all Stocks(After Normalizing)')
-    st.plotly_chart(CAPM_Functions.interactive_plot(CAPM_Functions.normalize(stocks_df)))
+    with col1:
+        st.markdown('### Calculated Beta Value')
+        st.dataframe(beta_df, use_container_width= True)
+        
+    rf = 0
+    rm = stocks_daily_return['sp500'].mean()*252
+    
+    return_df = pd.DataFrame()
+    return_value = []
+    for stock, value in beta.items():
+        return_value.append(str(round(rf + (value*(rm - rf)),2)))
+    return_df['Stock'] = stocks_list
+    
+    return_df['Return Value'] = return_value
+    
+    with col2:
+        st.markdown('### Calculated Return using CAPM')
+        
+        st.dataframe(return_df, use_container_width= True)
+except:
+    st.write("Please select valid Input")
+    
